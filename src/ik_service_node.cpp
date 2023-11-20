2fd56080f5be712655683a4dc22810a5739465a9
josestark0905 <143913141+josestark0905@users.noreply.github.com>
2023-11-19 19:59:47 -0500

    Add files via upload

--- src/ik_service_node.cpp
+++ src/ik_service_node.cpp
@@ -1,11 +1,30 @@
 #include "ros/ros.h"
 #include "ik_service/PoseIK.h"
+#include "ur_kinematics/ur_kin.h"
+#include <iostream>
 
-bool pose_ik(ik_service::PoseIK::Request  &req,
+bool pose_ik(ik_service::PoseIK::Request &req,
          ik_service::PoseIK::Response &res)
 {
+  double q[] = {3.14, -1.13, 1.51, 3.77, -1.51, 0};
+  double T[4][4];
+  ur_kinematics::forward((double *)&q[0], (double *)&T[0][0]);
+  T[0][3] = req.part_pose.position.x;
+  T[1][3] = req.part_pose.position.y;
+  T[2][3] = req.part_pose.position.z;
+  
+  int num_sol;
+  double q_sols[8][6];
+  num_sol = ur_kinematics::inverse(&T[0][0], &q_sols[0][0], 0.0);
+  
+  ROS_INFO("GET %i solutions", num_sol);
   ROS_INFO("SEND THE NUMBER OF SOLUTIONS");
-  res.num_sols = -1;
+  res.num_sols = num_sol;
+  for(int i=0;i<8;++i){
+  	for(int j=0;j<6;++j){
+  		res.joint_solutions[i].joint_angles[j] = q_sols[i][j];
+  	}
+  }
   return true;
 }
 
