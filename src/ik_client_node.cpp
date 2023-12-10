3ea505b95a73768bbe22a5be9e0e47c3121a0190
josestark0905 <143913141+josestark0905@users.noreply.github.com>
2023-12-10 09:46:15 -0500

    Add files via upload

--- src/ik_client_node.cpp
+++ src/ik_client_node.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "ik_client");
+  ros::init(argc, argv, "ik_client_node");
   
   ik_service::PoseIK ik_pose;
   geometry_msgs::Pose part_pose;
@@ -12,6 +12,7 @@ int main(int argc, char **argv)
   part_pose.position.x = 0.5;
   part_pose.position.y = 0;
   part_pose.position.z = 0;
+  ROS_WARN("Ask the server x = %f, y = %f, z = %f", part_pose.position.x, part_pose.position.y, part_pose.position.z);
   
   ik_pose.request.part_pose = part_pose;
 
@@ -21,7 +22,13 @@ int main(int argc, char **argv)
   if (client.call(ik_pose))
   {
     ROS_INFO("Call to ik_service returned [%i] solutions", ik_pose.response.num_sols);
-    
+    for(int i=0; i<ik_pose.response.num_sols; i++){
+    	ROS_INFO("------------------------------------------");
+    	ROS_INFO("Solution %d:", i+1);
+    	for(int j=0; j<6; j++){
+    		ROS_INFO("%f", ik_pose.response.joint_solutions[i].joint_angles[j]);
+    	}
+    }
   }
   else
   {
--- src/ik_service_node.cpp
+++ src/ik_service_node.cpp
@@ -17,8 +17,8 @@ bool pose_ik(ik_service::PoseIK::Request &req,
   double q_sols[8][6];
   num_sol = ur_kinematics::inverse(&T[0][0], &q_sols[0][0], 0.0);
   
-  ROS_INFO("GET %i solutions", num_sol);
-  ROS_INFO("SEND THE NUMBER OF SOLUTIONS");
+  ROS_WARN("GET %i solutions", num_sol);
+  ROS_WARN("SEND THE NUMBER OF SOLUTIONS");
   res.num_sols = num_sol;
   for(int i=0;i<8;++i){
   	for(int j=0;j<6;++j){
@@ -30,7 +30,7 @@ bool pose_ik(ik_service::PoseIK::Request &req,
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "add_two_ints_server");
+  ros::init(argc, argv, "ik_service_node");
   ros::NodeHandle n;
 
   ros::ServiceServer service = n.advertiseService("pose_ik", pose_ik);
